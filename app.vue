cd594a18c9b2352861089a5e98973c36f78f09a9
Luke C Hartman <luke@lukecarlhartman.com>
2025-01-03 12:01:11 -0800

    components https://nuxt.com/docs/getting-started/views#components

--- app.vue
+++ app.vue
@@ -1,8 +1,7 @@
 <!-- By default, Nuxt will treat this file as the entrypoint and render its content for every route of the application. -->
 <template>
-  <h1>
-    Check the tab {{  }}
-  </h1>
+  <h1>Check the tab</h1>
+  <AppAlert> This is an auto-imported component. </AppAlert>
 </template>
 
 <script setup lang="ts">
@@ -10,8 +9,8 @@ const config = useRuntimeConfig()
 // console.log(config.mySecret);
 // console.log(config.nuxtApiKeyTest);
 const appConfig = useAppConfig()
-console.log(appConfig.theme);
-const newAppConfig = { foo: 'baz'}
+// console.log(appConfig.theme);
+const newAppConfig = { foo: "baz" }
 updateAppConfig(newAppConfig)
-console.log(appConfig);
+// console.log(appConfig);
 </script>
