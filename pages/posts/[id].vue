3b468cec94defd9acb341e9ce7a26f6dd2e8b7ce
Luke C Hartman <luke@lukecarlhartman.com>
2025-01-03 14:04:22 -0800

    route params https://nuxt.com/docs/getting-started/routing#route-parameters

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -4,3 +4,10 @@
 		<NuxtLink to="/"></NuxtLink>
 	</section>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route.params.id)
+</script>
