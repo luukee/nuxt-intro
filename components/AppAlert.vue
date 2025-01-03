71b37b5e1c5c703748b4d7da97f0b0a51abf6e6e
Luke C Hartman <luke@lukecarlhartman.com>
2025-01-03 12:04:11 -0800

    components descrip comment https://nuxt.com/docs/getting-started/views#components

--- components/AppAlert.vue
+++ components/AppAlert.vue
@@ -1,3 +1,10 @@
+<!-- 
+Most components are reusable pieces of the user interface, 
+like buttons and menus. In Nuxt, you can create these components 
+in the components/ directory, and they will be automatically 
+available across your application without having to explicitly 
+import them.
+-->
 <template>
   <span>
     <slot />
