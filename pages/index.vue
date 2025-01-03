34ba587e66a28915590cdf24baadf7a8d40f217a
Luke C Hartman <luke@lukecarlhartman.com>
2025-01-03 12:22:26 -0800

    nuxtLink https://nuxt.com/docs/api/components/nuxt-link

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component </AppAlert>
+    <AppAlert> This is an auto-imported component <NuxtLink to="/about">About Page</NuxtLink></AppAlert>
+		<NuxtLink to="/posts/1">
+			<p>Post 1</p>
+		</NuxtLink>
   </div>
 </template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,5 @@
+<template>
+	<section>
+		<h1>Post: post title</h1>
+	</section>
+</template>
