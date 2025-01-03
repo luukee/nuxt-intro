1ccdc784e2ab270ae836e2c58955c1690057b0ed
Luke C Hartman <luke@lukecarlhartman.com>
2025-01-03 12:30:22 -0800

    external routing https://nuxt.com/docs/api/components/nuxt-link#external-routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component <NuxtLink to="/about">About Page</NuxtLink></AppAlert>
-		<NuxtLink to="/posts/1">
-			<p>Post 1</p>
-		</NuxtLink>
+    <AppAlert>
+      This is an auto-imported component <NuxtLink to="/about">About Page</NuxtLink></AppAlert
+    >
+    <NuxtLink to="/posts/1">
+      <p>Post 1</p>
+    </NuxtLink>
+    <!-- External -->
+    <NuxtLink to="/the-important-report.pdf" external>
+      <p>Download Report</p>
+    </NuxtLink>
   </div>
 </template>
--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,5 +1,6 @@
 <template>
 	<section>
 		<h1>Post: post title</h1>
+		<NuxtLink to="/"></NuxtLink>
 	</section>
 </template>
