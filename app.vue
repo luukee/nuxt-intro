84c817085083a8677a4c4ccb96c4c753e6a7c461
Luke C Hartman <luke@lukecarlhartman.com>
2025-01-03 15:05:09 -0800

    return to dynamic site title

--- app.vue
+++ app.vue
@@ -22,12 +22,9 @@ updateAppConfig(newAppConfig)
 
 // useHead
 useHead({
-  // or as a function
-  titleTemplate: (productCategory) => {
-    return productCategory
-      ? `${productCategory} - Site Title as a function`
-      : 'Site Title'
-  },
+  // as a string,
+  // where `%s` is replaced with the title
+  titleTemplate: '%s - Dynamic Site Title',
   meta: [
     { 
       name: 'description', 
@@ -68,3 +65,15 @@ useSeoMeta({
 })
 
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
